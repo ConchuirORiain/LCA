829d0c65c4276c27e4aef74be7f06ca1bfd39b14
Conor <ryanc59@tcd.ie>
2019-09-22 17:44:32 +0100

    added lca function but doesn't seem to work correctly yet

--- LCA.c
+++ LCA.c
@@ -29,12 +29,27 @@ struct treeNode *newNode(int data){
 	return result;
 };	
 
+struct treeNode *lca(struct treeNode* root, int n1, int n2) 
+{ 
+    if (root == NULL) return NULL; 
+  
+    if (root->data > n1 && root->data > n2) 
+        return lca(root->left, n1, n2); 
+  
+    if (root->data < n1 && root->data < n2) 
+        return lca(root->right, n1, n2); 
+  
+    return root; 
+}
+ 
 int main(){
 	struct tree *graph = newEmptyTree();
 	graph->head = newNode(1);
 	graph->head->left = newNode(2);
 	graph->head->right = newNode(3);
+	struct treeNode *ancestor = lca(graph->head, 2, 3);
 	printf("root %d\n", graph->head->data);
 	printf("left %d\n", graph->head->left->data);
 	printf("right %d\n", graph->head->right->data);
+	printf("lca: %d\n", ancestor -> data);
 }
