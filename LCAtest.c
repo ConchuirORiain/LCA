9ebc49d9bf919b06bac72ac8d7f0415b093b51f1
Conor <ryanc59@tcd.ie>
2019-09-23 21:40:39 +0100

    added left leaning graph to test

--- LCAtest.c
+++ LCAtest.c
@@ -4,10 +4,13 @@
 struct tree *graph;
 struct tree *nullGraph;
 struct tree *rightLeanGraph;
+struct tree *leftLeanGraph;
+
 void setUp(void){
 	graph = newEmptyTree();
 	nullGraph = newEmptyTree();
 	rightLeanGraph = newEmptyTree();
+	leftLeanGraph = newEmptyTree();
 
 	graph->head = newNode(1);
 	graph->head->left = newNode(2);
@@ -19,6 +22,13 @@ void setUp(void){
 	rightLeanGraph->head->right->right->right = newNode(4);
 	rightLeanGraph->head->right->right->right->right = newNode(5);
 	rightLeanGraph->head->right->right->right->right->right = newNode(6);
+
+	leftLeanGraph->head = newNode(1);
+	leftLeanGraph->head->left = newNode(2);
+	leftLeanGraph->head->left->left = newNode(3);
+	leftLeanGraph->head->left->left->left = newNode(4);
+	leftLeanGraph->head->left->left->left->left = newNode(5);
+	leftLeanGraph->head->left->left->left->left->left = newNode(6);
 }
 
 void tearDown(void){
@@ -28,6 +38,7 @@ void testLCAFunction(void){
 	TEST_ASSERT_EQUAL_MESSAGE(1,lca(graph->head,2,3)->data,"Basic graph, answer should be 1");
 	TEST_ASSERT_NULL_MESSAGE(lca(nullGraph->head,2,3),"tree with no nodes, answer should be null");
 	TEST_ASSERT_EQUAL_MESSAGE(5,lca(rightLeanGraph->head,5,6)->data,"long right leaning graph, answer should be 5");
+	TEST_ASSERT_EQUAL_MESSAGE(5,lca(leftLeanGraph->head,5,6)->data,"long left leaning graph, answer should be 5");
 }
 
 int main(void){
