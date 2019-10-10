83674783ace9cb2acb270d41cff8feae98b24687
Conor <ryanc59@tcd.ie>
2019-10-10 15:45:39 +0100

    added another test for a DAG with two sets of nodes

--- LCAtest.c
+++ LCAtest.c
@@ -8,6 +8,7 @@ struct graph *rightLeanGraph;
 struct graph *leftLeanGraph;
 struct graph *complexGraph;
 struct graph *simpleDAG;
+struct graph *twoNodeSetsDAG;
 
 void setUp(void){
 	nullGraph = newEmptyGraph();
@@ -17,6 +18,7 @@ void setUp(void){
 	leftLeanGraph = newEmptyGraph();
 	complexGraph = newEmptyGraph();
 	simpleDAG = newEmptyGraph();
+	twoNodeSetsDAG = newEmptyGraph();
 
 	graph->head = newNode(1);
 	graph->head->left = newNode(2);
@@ -60,6 +62,21 @@ void setUp(void){
 	simpleDAG->head->right->right->left = simpleDAG->head->left->left->right;
 	simpleDAG->head->right->right->left->left = newNode(7);
 
+	twoNodeSetsDAG->head = newNode(1);
+	twoNodeSetsDAG->head->left = newNode(2);
+	twoNodeSetsDAG->head->right = newNode(3);
+	twoNodeSetsDAG->head->left->right = newNode(4);
+	twoNodeSetsDAG->head->right->left = newNode(5);
+	twoNodeSetsDAG->head->left->right->right = newNode(6); 
+	twoNodeSetsDAG->head->right->left->left = twoNodeSetsDAG->head->left->right->right; 
+	twoNodeSetsDAG->head->right->left->left->left = newNode(7);
+	twoNodeSetsDAG->head->right->left->left->right = newNode(8);
+	twoNodeSetsDAG->head->right->left->left->left->left = newNode(9);
+	twoNodeSetsDAG->head->right->left->left->right->right = newNode(10);
+	twoNodeSetsDAG->head->right->left->left->left->left->right = newNode(11);
+	twoNodeSetsDAG->head->right->left->left->right->right->left =
+			 twoNodeSetsDAG->head->right->left->left->left->left->right;
+	
 }
 
 void tearDown(void){
@@ -81,16 +98,26 @@ void testLCAFunctionNULLDAG(void){
 }
 void testLCAFunctionSimpleDAG(void){
 	TEST_ASSERT_EQUAL_PTR_MESSAGE(simpleDAG->head,lca(simpleDAG->head,2,3),"Simple query, answer should be 1");
+	char str[100];
+	sprintf(str,"values of lca is %d",lca(simpleDAG->head,6,7)->data);
 	TEST_ASSERT_EQUAL_PTR_MESSAGE(simpleDAG->head->left->left->right,
-		lca(simpleDAG->head,6,7),"testing one way through cycle");
+		lca(simpleDAG->head,6,7),str);
 	TEST_ASSERT_EQUAL_PTR_MESSAGE(simpleDAG->head->right->right->left,
 		lca(simpleDAG->head,6,7),"testing second way through cycle");
+}
+
+void testLCAFunctionTwoNodesSetDAG(void){
+	TEST_ASSERT_EQUAL_PTR_MESSAGE(twoNodeSetsDAG->head,lca(twoNodeSetsDAG->head,2,3),"answer should be 1");
+	TEST_ASSERT_EQUAL_PTR_MESSAGE(twoNodeSetsDAG->head->left->right->right,
+			lca(twoNodeSetsDAG->head,7,8),"answer should be 6");
+	
 }
 
 int main(void){
 	UNITY_BEGIN();
 	RUN_TEST(testLCAFunction);
 	RUN_TEST(testLCAFunctionNULLDAG);
-	RUN_TEST(testLCAFunctionSimpleDAG);
+//	RUN_TEST(testLCAFunctionSimpleDAG);
+	RUN_TEST(testLCAFunctionTwoNodesSetDAG);
 	return UNITY_END();
 }
